a38714560814e30b0d3c927e81a514edb87c77f8
sunrxse66 <96800308+sunrxse66@users.noreply.github.com>
2024-03-13 23:42:15 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -72,6 +72,23 @@ function closeModal(modal) {
   }
 }
 
+// Добавляем обработчик событий для закрытия модального окна
+function setupCloseModalOnOutsideClick() {
+  window.addEventListener('click', function(event) {
+      if (event.target.classList.contains('modal')) {
+          closeModal(event.target);
+      }
+  });
+
+  // Обработчик касаний для закрытия модального окна (для мобильных устройств)
+  window.addEventListener('touchend', function(event) {
+      if (event.target.classList.contains('modal')) {
+          closeModal(event.target);
+          event.preventDefault(); // Предотвращаем выполнение последующих событий "click"
+      }
+  });
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   // Добавляем обработчики для открытия модальных окон
   var boostsButton = document.querySelector('.menu-cont.boost');
