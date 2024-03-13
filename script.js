2f017ee7805233ffd3bab91353c140c3b017294a
sunrxse66 <96800308+sunrxse66@users.noreply.github.com>
2024-03-13 14:56:10 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,14 +3,3 @@ window.addEventListener('touchmove', function(e) {
     e.preventDefault();
   }, { passive: false });
   
-  // Запрет событий `wheel` для предотвращения скролла мышью
-  window.addEventListener('wheel', function(e) {
-    e.preventDefault();
-  }, { passive: false });
-  
-  // Запрет событий `keydown` для предотвращения скролла клавишами
-  window.addEventListener('keydown', function(e) {
-    if(['Space','ArrowUp','ArrowDown','ArrowLeft','ArrowRight'].indexOf(e.code) > -1) {
-      e.preventDefault();
-    }
-  }, false);
